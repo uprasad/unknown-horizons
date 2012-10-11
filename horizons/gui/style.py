b5285431b0e4a83e0ebac6f2e0d72f9d6262af10
vdaras <vasileiosdaras@gmail.com>
2012-10-11 21:42:46 +0200

    Made Unknown Horizons work with modularized Pychan.

--- horizons/gui/style.py
+++ horizons/gui/style.py
@@ -19,7 +19,7 @@
 # 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 # ###################################################
 
-from fife.fife import Color
+from fife.fifechan import Color
 
 STYLES = {
 'default': {
